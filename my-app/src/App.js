0a484faab79e059cf93acebef05faa26999a5e01
KainChin <khanhtrinh882004@gmail.com>
2025-06-09 18:47:04 +0700

    điều chỉnh
    chỉnh sửa trang hompage và trang  hội viên

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,4 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import Header from './components/Header';
-import Banner from './components/Banner';
-import StatusBar from './components/StatusBar';
-import News from './components/News';
-import Events from './components/Events';
-import WhyChoose from './components/WhyChoose';
-import CallToAction from './components/CallToAction';
-import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Banner />
-      <StatusBar />
-      <News />
-      <Events />
-      <WhyChoose />
-      <CallToAction />
-      <Footer />
-    </div>
-=======
-// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import HoiVien from "./pages/HoiVien";
@@ -35,7 +11,6 @@ function App() {
         <Route path="/hoivien" element={<HoiVien />} />
       </Routes>
     </Router>
->>>>>>> Stashed changes
   );
 }
 
