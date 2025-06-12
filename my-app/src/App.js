0f98eabf70cb4d46cb5adc68ef4b0323ffec5b97
LBK04 <khanhlbse183944@fpt.edu.vn>
2025-06-12 10:29:59 +0700

    khanh.bao

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,9 +4,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import HoiVien from "./pages/HoiVien";
-=======
-=======
->>>>>>> c605bcec07dcba7239145b0db022cfb1acaf1919
+
 // src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -14,10 +12,7 @@ import HomePage from './pages/HomePage';
 import HoiVien from './pages/HoiVien';
 import DichVu from './pages/DichVu';
 <<<<<<< HEAD
->>>>>>> c605bcec07dcba7239145b0db022cfb1acaf1919
-=======
->>>>>>> c605bcec07dcba7239145b0db022cfb1acaf1919
-
+    
 function App() {
   return (
     <Router>
