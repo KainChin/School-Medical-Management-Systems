7fdeecc2fed2753c18306617f75797c5f113154e
KainChin <khanhtrinh882004@gmail.com>
2025-07-14 05:31:44 +0700

    Update INotificationService.java

--- src/main/java/com/school_medical/school_medical_management_system/services/INotificationService.java
+++ src/main/java/com/school_medical/school_medical_management_system/services/INotificationService.java
@@ -8,5 +8,4 @@ public interface INotificationService {
     public void sendConsentNotificationsForBatch(int batchId, String content, String type, String consentType);
     void updateConsent(Long notificationId, Boolean consentStatus);
     List<Notification> getNotificationsByParentId();
-    List<Notification> getAllNotificationsOrderByDateSentDesc();
 }
