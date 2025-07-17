32f137564dcee0fe751bf3dafdac28ddca061a61
KainChin <khanhtrinh882004@gmail.com>
2025-07-17 10:30:32 +0700

    notification

--- src/main/java/com/school_medical/school_medical_management_system/services/INotificationService.java
+++ src/main/java/com/school_medical/school_medical_management_system/services/INotificationService.java
@@ -1,6 +1,7 @@
 package com.school_medical.school_medical_management_system.services;
 
 import com.school_medical.school_medical_management_system.repositories.entites.Notification;
+import jakarta.mail.MessagingException;
 
 import java.util.List;
 
@@ -8,4 +9,6 @@ public interface INotificationService {
     public void sendConsentNotificationsForBatch(int batchId, String content, String type, String consentType);
     void updateConsent(Long notificationId, Boolean consentStatus);
     List<Notification> getNotificationsByParentId();
+    void sendNotificationToParent(String email, String notificationContent) throws MessagingException;
+    List getEmailOfParent;
 }
